0495a4804b2f8d40f44fa40f97d58892ac78a990
Penwolfe <idgoodrich@ualr.edu>
2022-03-15 17:49:49 -0500

    Backend and Mail
    Added the backend to the login and registration forms, sending emails now works

--- app.py
+++ app.py
@@ -1,9 +1,43 @@
-from flask import Flask
-
+from flask import Flask, render_template, url_for
+from flask_mail import Mail, Message
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+app.config['SECRET_KEY']='c9086aeae8e7451dd9f38272ee4f315a'
 
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'UALR.Capstone.Team42@gmail.com'
+app.config['MAIL_PASSWORD'] = 'ualrcs42'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+mail = Mail(app)
 
 @app.route('/')
 def hello():
     return 'Hello, World!'
+
+@app.route('/email')
+def sendEmail():
+    msg = Message('Testing the email stuff', 
+                  sender = 'UALR.Capstone.Team42@gmail.com',
+                  recipients = ['UALR.Capstone.Team42@gmail.com'])
+    msg.body = "testing email stuff"
+    mail.send(msg)
+    return 'Message Sent!'
+
+@app.route('/register')
+def register():
+    form = RegistrationForm
+    if form.validate_on_submit():
+        #send email to form.email.data
+        print('hello world')
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/login')
+def login():
+    form=LoginForm
+    return render_template('login.html', title='Log In', form=form)
+
+
+
 
---
+++ forms.py
@@ -0,0 +1,16 @@
+﻿from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=5, max=20)])
+    email = StringField('Email', validators=[DataRequired(),Email()])
+    accessLevel = StringField('AccessLevel', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirmPassword = PasswordField('confirmPassword', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(),Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
