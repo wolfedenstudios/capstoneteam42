662d1897632c09b722f40300233274302fce6b22
idgoodrich <35702432+idgoodrich@users.noreply.github.com>
2022-04-22 14:25:41 -0500

    add requirements and database models

--- capstone/models.py
+++ capstone/models.py
@@ -22,3 +22,44 @@ class accounts(db.Model, UserMixin):
 
     def __repr__(self):
        return f"User('{self.username}', '{self.email}')"
+
+class instructors(db.Model):
+    LName = db.Column(db.String(20), unique=True, nullable = False, primary_key = True)
+    MaxLoad = db.Column(db.Integer, nullable = False)
+    Disciplines = db.Column(db.String(99), nullable = False)
+    Course_code_1 = db.Column(db.Integer, nullable = False)
+    Course_code_2 = db.Column(db.Integer, nullable = False)
+    Course_code_3 = db.Column(db.Integer, nullable = False)
+    Course_code_4 = db.Column(db.Integer, nullable = False)
+    Schedule_Day_1 = db.Column(db.String(30), nullable = False)
+    Schedule_Day_2 = db.Column(db.String(30), nullable = False)
+    Schedule_Day_3 = db.Column(db.String(30), nullable = False)
+    Schedule_Day_4 = db.Column(db.String(30), nullable = False)
+    Schedule_Day_5 = db.Column(db.String(30), nullable = False)
+    CurrentLoad = db.Column(db.Integer, nullable=False)
+
+
+
+class sections(db.Model):
+    Code = db.Column(db.Integer, unique = True, nullable = False, primary_key = True)
+    DepartmentCode = db.Column(db.String(4), nullable = False)
+    Day = db.Column(db.String(5), nullable = False)
+    Length = db.Column(db.Integer, nullable = False)
+    StartTime = db.Column(db.Integer, nullable = False)
+    Disciplines = db.Column(db.String(99), nullable = False)
+    Periods = db.Column(db.Integer, nullable = False)
+    Name = db.Column(db.String(50), nullable = False)
+    instructor = db.Column(db.String(20), nullable = False)
+
+class output_schedule(db.Model):
+    Code = db.Column(db.Integer, unique = True, nullable = False, primary_key = True)
+    DepartmentCode = db.Column(db.String(4), nullable = False)
+    Day = db.Column(db.String(5), nullable = False)
+    Length = db.Column(db.Integer, nullable = False)
+    StartTime = db.Column(db.Integer, nullable = False)
+    Disciplines = db.Column(db.String(99), nullable = False)
+    Periods = db.Column(db.Integer, nullable = False)
+    Name = db.Column(db.String(50), nullable = False)
+    instructor = db.Column(db.String(20), nullable = False)
+    valid = db.Column(db.Boolean, nullable = False)
+ 
